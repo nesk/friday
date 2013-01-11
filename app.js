ed0066a36408998bb925470626489d79234f58d9
Johann Pardanaud <pardanaud.j@gmail.com>
2013-01-12 00:30:00 +0100

    Forgot to load the configuration and the manager module

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express'),
     routes = require('./routes'),
     http = require('http'),
-    path = require('path');
+    path = require('path'),
+    yaml = require('yamljs');
 
-var app = express();
+var app = express(),
+    config = yaml.load('config.yml'), // Loads the app configuration
+    Manager = require(path.join(__dirname, 'lib/manager')).init(config); // Initiates the Manager with the app configuration
 
 app.configure(function(){
     app.set('port', 3000);
