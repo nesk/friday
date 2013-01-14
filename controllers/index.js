cf1c6d1bb8b5935b8eedf7a917eeca46fc6821ca
Johann Pardanaud <pardanaud.j@gmail.com>
2013-01-14 16:57:05 +0100

    The content is now changed when the user is authenticated or not

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,7 @@
 exports.index = function(req, res) {
-    res.render('index');
+    res.render('index', {
+        authenticated: !!req.session.id,
+    });
 };
 
 exports.auth = function(req, res) {
