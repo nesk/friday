b907af8e60f532de473438de58abdf54facf6c18
Johann Pardanaud <pardanaud.j@gmail.com>
2013-01-15 23:23:13 +0100

    Client-side : flash messages are now erased after 5s and can be queued

--- public/js/friday.js
+++ public/js/friday.js
@@ -1,7 +1,7 @@
 (function() {
     
     /*
-     * Flash
+     * Animation
      */
 
     var day = new Date().getDay(),
@@ -9,5 +9,36 @@
         index = +(day != 5);
     
     objects[index].style.display = 'block';
+
+    /*
+     * Flash messages
+     */
+
+    var msgList = document.getElementById('msg-list');
+
+    var flashMsg = {
+
+        applyTimeout: function(node) {
+            setTimeout(function() {
+                node.className += ' hide-msg';
+                setTimeout(function() {
+                    msgList.removeChild(node);
+                }, 400);
+            }, 5000);
+        },
+
+        add: function(text) {
+            var node = document.createElement('div');
+            node.className = 'msg';
+            node.innerHTML = text;
+            msgList.appendChild(node);
+            this.applyTimeout(node);
+        }
+
+    };
+
+    if(msgList.childNodes.length) {
+        flashMsg.applyTimeout(msgList.childNodes[0]); // There can be only one message when the page is loading, a loop is unnecessary.
+    }
 
 })();
